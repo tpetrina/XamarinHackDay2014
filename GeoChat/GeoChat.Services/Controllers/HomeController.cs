6acef1ae2d1f818a9fa3f9327bd44199a31234af
Petar Kovačević <petar.kovacevic@axilis.hr>
2014-10-25 15:21:13 +0200

    Entity framework referenced in services project

--- GeoChat/GeoChat.Services/Controllers/HomeController.cs
+++ GeoChat/GeoChat.Services/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using GeoChat.Models;
+using GeoChat.Persistance;
 
 namespace GeoChat.Services.Controllers
 {
